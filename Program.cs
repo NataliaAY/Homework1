af4638ccb415bafbf3ddcf8abb1e6f2761b8d299
Natalia <averegina@male.ru>
2022-11-23 10:07:38 +0300

    решена задача 1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num_X= Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num_Y = Convert.ToInt32(Console.ReadLine());
+
+if (num_X > num_Y)
+{
+Console.WriteLine("Первое число " + num_X + " больше чем второе " + num_Y);
+}
+else
+{
+Console.WriteLine("Второе число " + num_Y + " больше чем первое " + num_X);
+}
